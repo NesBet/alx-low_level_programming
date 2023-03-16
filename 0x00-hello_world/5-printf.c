d9ec8d9b651caba22dc592b9230b231f7c8d21e5
NesBet <nehmz254@gmail.com>
2023-03-17 00:35:32 +0300

    Future

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 if program executes
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
