1f2054124effdafb2ef6b2ded3c636ecc97d9c3d
NesBet <nehmz254@gmail.com>
2023-03-21 15:51:01 +0300

    Vibes on Vibes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - prints the sign of a number.
  *
- * n is the number to be checked.
+ *@n is the number to be checked.
  *
  * Return: 1 and prints + if n is greater than zero;
  *0 and prints 0 if n is zero;
