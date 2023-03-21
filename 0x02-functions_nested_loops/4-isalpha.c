75b44df862b7b0b756ff827b73ac2f18be844980
NesBet <nehmz254@gmail.com>
2023-03-21 15:55:51 +0300

    Vibes on Vibes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - checks for alphabetic character both small and capital letters.
  *
- * c is the character to be checked.
+ * @c: is the character to be checked.
  *
  * Return: 1 if c is a letter, 0 if otherwise.
  */
