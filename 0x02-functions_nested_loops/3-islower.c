cfa086ef95d6f7d6e4e4d55526e96d18cd2ccc67
NesBet <nehmz254@gmail.com>
2023-03-21 15:58:24 +0300

    Vibes on Vibes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - checks for a lowercase character.
  *
- * c is the character to be checked.
+ * @c: is the character to be checked.
  * Return: 1 if c is lowercase, 0 if otherwise.
  */
 int _islower(int c)
