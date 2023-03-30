2ad64bf3d9a6a70b6f44db20471330117b04dd89
NesBet <nehmz254@gmail.com>
2023-03-30 23:24:45 +0300

    AfroCodes

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string to use rot13.
+ * @s: Input string.
+ * Return: Pointer to destination.
+ */
+
+char *rot13(char *s)
+{
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -6,3 +6,7 @@
 5-string_toupper.c__Function that changes all lowercase letters of a string to uppercase.
 6-cap_string.c___Function that capitalizes all words of a string.
 7-leet.c___Function that encodes a string into 1337.
+100-rot13.c___Function that encodes a string using rot13.
+101-print_number.c___Program prints a[2] = 98 after adding one line.
+103-infinite_add.c___Function that adds two numbers.
+104-print_buffer.c___Function that prints a buffer.
