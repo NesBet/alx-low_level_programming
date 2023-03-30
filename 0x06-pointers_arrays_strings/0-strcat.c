6d8428fe20747c94959ef350cae3da5f35951036
NesBet <nehmz254@gmail.com>
2023-03-30 22:27:02 +0300

    AfroCodes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,26 @@
-include "main.h"
-
+#include "main.h"
 /**
- * _strcat - Concatenates two strings.
- * @dest: Pointer to destination string.
- * @src: Source string.
- *
- * Return: Destination string.
+ * _strcat - Concatenates two strings,
+ * @dest: Destination.
+ * @src: Source.
+ * Return: Pointer to destination.
  */
 char *_strcat(char *dest, char *src)
 {
-    int dest_len = 0, i = 0;
+	int count = 0, count2 = 0;
 
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
-
-
-    while (src[i] != '\0')
-    {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
-
-
-    dest[dest_len + i] = '\0';
-
-
-    return dest;
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
-
