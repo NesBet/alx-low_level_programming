a644938037b4e88ac94ca60e8ed49336c62f75de
NesBet <nehmz254@gmail.com>
2023-04-11 14:15:59 +0300

    AfroCodes

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2 dimensional array.
+ * @grid: A multi-dimensional array made up of char.
+ * @height: Height of the array.
+ *
+ * Return: NULL
+ */
+void free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string.
+ *
+ * Return: Pointer the array of integers.
+ */
+char **strtow(char *str)
+{
+	char **arout;
+	unsigned int k, height, i, j, b1;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (k = height = 0; str[k] != '\0'; k++)
+		if (str[k] != ' ' && (str[k + 1] == ' ' || str[k + 1] == '\0'))
+			height++;
+	arout = malloc((height + 1) * sizeof(char *));
+	if (arout == NULL || height == 0)
+	{
+		free(arout);
+		return (NULL);
+	}
+	for (i = b1 = 0; i < height; i++)
+	{
+		for (k = b1; str[k] != '\0'; k++)
+		{
+			if (str[k] == ' ')
+				b1++;
+			if (str[k] != ' ' && (str[k + 1] == ' ' || str[k + 1] == '\0'))
+			{
+				arout[i] = malloc((k - b1 + 2) * sizeof(char));
+				if (arout[i] == NULL)
+				{
+					free_grid(arout, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+		for (j = 0; b1 <= k; b1++, j++)
+			arout[i][j] = str[b1];
+		arout[i][j] = '\0';
+	}
+	arout[i] = NULL;
+	return (arout);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,3 +3,5 @@
 2-str_concat.c___Function that concatenates two strings.
 3-alloc_grid.c___Function that returns a pointer to a 2 dimensional array of integers.
 4-free_grid.c___Function that frees a 2 dimensional grid previously created by your alloc_grid function.
+100-argstostr.c___Function that concatenates all the arguments of your program.
+101-strtow.c___Function that splits a string into words.
