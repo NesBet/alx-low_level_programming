eb48a64dd41fd81b124cb1b43eca55e6479c5309
NesBet <nehmz254@gmail.com>
2023-04-14 01:40:33 +0300

    AfroCodes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,12 +9,11 @@
  *
  * Return: Pointer to the allocated memory location.
  * Return a status value of 98 if malloc fails.
- *
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *out;
-	unsigned int l1, l2, lout, i;
+	char *cout;
+	unsigned int lc1, lc2, lcout, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,29 +21,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (l1 = 0; s1[l1] != '\0'; l1++)
+	for (lc1 = 0; s1[lc1] != '\0'; l1++)
 		;
 
-	for (l2 = 0; s2[l2] != '\0'; l2++)
+	for (lc2 = 0; s2[lc2] != '\0'; l2++)
 		;
 
-	if (n > l2)
-		n = l2;
+	if (n > lc2)
+		n = lc2;
 
-	lout = l1 + n;
+	lcout = lc1 + n;
 
-	out = malloc(lout + 1);
+	cout = malloc(lcout + 1);
 
-	if (out == NULL)
+	if (cout == NULL)
 		return (NULL);
 
-	for (i = 0; i < lout; i++)
-		if (i < l1)
-			out[i] = s1[i];
+	for (i = 0; i < lcout; i++)
+		if (i < lc1)
+			cout[i] = s1[i];
 		else
-			out[i] = s2[i - ls1];
+			cout[i] = s2[i - lc1];
 
-	out[i] = '\0';
+	cout[i] = '\0';
 
-	return (out);
+	return (cout);
 }
