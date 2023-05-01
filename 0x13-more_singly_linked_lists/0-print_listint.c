121138ec69e8490281ec0c6dd732b27e6f4a79e8
NesBet <nehmz254@gmail.com>
2023-05-01 18:23:14 +0300

    AfroCodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
+ * print_listint - Prints all the elements of
+ * a listint_t list.
  * @h: Top of a list.
  *
  * Return: Total number of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		n++;
+		nodes++;
 	}
-	return (n);
+	return (nodes);
 }
