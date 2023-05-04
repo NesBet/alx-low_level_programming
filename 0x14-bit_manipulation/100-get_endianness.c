9cd935a416186649c80e2ef8e439388d3d317756
NesBet <nehmz254@gmail.com>
2023-05-05 01:19:05 +0300

    Advanced

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	unsigned int ui;
+	char *c;
+
+	ui = 1;
+	c = (char *) &ui;
+
+	return ((int)*c);
+}
