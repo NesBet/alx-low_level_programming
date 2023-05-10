56a2df246df158ec88686514567d3abc720755aa
NesBet <nehmz254@gmail.com>
2023-05-10 17:19:55 +0300

    AfroCodes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,59 +1,246 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
 
 /**
- * print_error - prints an error message to stderr and exits with status code 98
- * @msg: the error message to print
+ * print_addr - Prints an address.
+ * @ptr: Pointer.
+ * Return: Null.
  */
-void print_error(char *msg)
+void print_addr(char *ptr)
 {
-    fprintf(stderr, "%s\n", msg);
-    exit(98);
+	int i;
+	int begin;
+	char sys;
+
+	printf("  Entry point address:               0x");
+
+	sys = ptr[4] + '0';
+	if (sys == '1')
+	{
+		begin = 26;
+		printf("80");
+		for (i = begin; i >= 22; i--)
+		{
+			if (ptr[i] > 0)
+				printf("%x", ptr[i]);
+			else if (ptr[i] < 0)
+				printf("%x", 256 + ptr[i]);
+		}
+		if (ptr[7] == 6)
+			printf("00");
+	}
+
+	if (sys == '2')
+	{
+		begin = 26;
+		for (i = begin; i > 23; i--)
+		{
+			if (ptr[i] >= 0)
+				printf("%02x", ptr[i]);
+
+			else if (ptr[i] < 0)
+				printf("%02x", 256 + ptr[i]);
+
+		}
+	}
+	printf("\n");
 }
 
 /**
- * main - displays the information contained in the ELF header at the start of an ELF file
- * @argc: the number of command-line arguments
- * @argv: an array of command-line arguments
- * Return: 0 on success, 98 on failure
+ * print_type - Prints the type of file.
+ * @ptr: Pointer.
+ * Return: NULL.
  */
-int main(int argc, char **argv)
+void print_type(char *ptr)
 {
-    int fd;
-    Elf64_Ehdr header;
+	char type = ptr[16];
 
-    if (argc != 2)
-        print_error("Usage: elf_header elf_filename");
+	if (ptr[5] == 1)
+		type = ptr[16];
+	else
+		type = ptr[17];
 
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-        print_error("Error opening file");
+	printf("  Type:                              ");
+	if (type == 0)
+		printf("NONE (No file type)\n");
+	else if (type == 1)
+		printf("REL (Relocatable file)\n");
+	else if (type == 2)
+		printf("EXEC (Executable file)\n");
+	else if (type == 3)
+		printf("DYN (Shared object file)\n");
+	else if (type == 4)
+		printf("CORE (Core file)\n");
+	else
+		printf("<unknown: %x>\n", type);
+}
 
-    if (read(fd, &header, sizeof(header)) != sizeof(header))
-        print_error("Error reading ELF header");
+/**
+ * print_osabi - Prints requirement osabi.
+ * @ptr: Pointer.
+ * Return: NULL.
+ */
+void print_osabi(char *ptr)
+{
+	char osabi = ptr[7];
 
-    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3)
-        print_error("Not an ELF file");
+	printf("  OS/ABI:                            ");
+	if (osabi == 0)
+		printf("UNIX - System V\n");
+	else if (osabi == 2)
+		printf("UNIX - NetBSD\n");
+	else if (osabi == 6)
+		printf("UNIX - Solaris\n");
+	else
+		printf("<unknown: %x>\n", osabi);
 
-    printf("Magic:   %02x %02x %02x %02x\n",
-           header.e_ident[EI_MAG0],
-           header.e_ident[EI_MAG1],
-           header.e_ident[EI_MAG2],
-           header.e_ident[EI_MAG3]);
-    printf("Class:   %d-bit\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
-    printf("Data:    %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-    printf("Version: %d\n", header.e_ident[EI_VERSION]);
-    printf("OS/ABI:  %s\n", header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX System V ABI" : "Other");
-    printf("ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
-    printf("Type:    %s\n", header.e_type == ET_EXEC ? "Executable file" : header.e_type == ET_DYN ? "Shared object file" : "Other");
-    printf("Entry point address: 0x%lx\n", header.e_entry);
+	printf("  ABI Version:                       %d\n", ptr[8]);
+}
 
-    close(fd);
-    return (0);
+
+/**
+ * print_version - Prints the version.
+ * @ptr: Pointer.
+ * Return: NULL.
+ */
+void print_version(char *ptr)
+{
+	int version = ptr[6];
+
+	printf("  Version:                           %d", version);
+
+	if (version == EV_CURRENT)
+		printf(" (current)");
+
+	printf("\n");
+}
+/**
+ * print_data - Prints data contained in file.
+ * @ptr: Pointer.
+ * Return: NULL.
+ */
+void print_data(char *ptr)
+{
+	char data = ptr[5];
+
+	printf("  Data:                              2's complement");
+	if (data == 1)
+		printf(", little endian\n");
+
+	if (data == 2)
+		printf(", big endian\n");
+}
+/**
+ * print_magic - Prints pointer info.
+ * @ptr: Pointer.
+ * Return: NULL.
+ */
+void print_magic(char *ptr)
+{
+	int bytes;
+
+	printf("  Magic:  ");
+
+	for (bytes = 0; bytes < 16; bytes++)
+		printf(" %02x", ptr[bytes]);
+
+	printf("\n");
+
+}
+
+/**
+ * check_sys - Checks system's version.
+ * @ptr: Pointer.
+ * Return: NULL.
+ */
+void check_sys(char *ptr)
+{
+	char sys = ptr[4] + '0';
+
+	if (sys == '0')
+		exit(98);
+
+	printf("ELF Header:\n");
+	print_magic(ptr);
+
+	if (sys == '1')
+		printf("  Class:                             ELF32\n");
+
+	if (sys == '2')
+		printf("  Class:                             ELF64\n");
+
+	print_data(ptr);
+	print_version(ptr);
+	print_osabi(ptr);
+	print_type(ptr);
+	print_addr(ptr);
+}
+
+/**
+ * check_elf - Verifies if it is an elf file.
+ * @ptr: Pointer.
+ * Return: 1 for an elf file, 0 if not.
+ */
+int check_elf(char *ptr)
+{
+	int addr = (int)ptr[0];
+	char E = ptr[1];
+	char L = ptr[2];
+	char F = ptr[3];
+
+	if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * main - Verifies code.
+ * @argc: Total number of arguments.
+ * @argv: Arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fd, ret_read;
+	char ptr[27];
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Err: file can not be open\n");
+		exit(98);
+	}
+
+	lseek(fd, 0, SEEK_SET);
+	ret_read = read(fd, ptr, 27);
+
+	if (ret_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
+		exit(98);
+	}
+
+	if (!check_elf(ptr))
+	{
+		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
+		exit(98);
+	}
+
+	check_sys(ptr);
+	close(fd);
+
+	return (0);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,46 +1,44 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-void error_file(int file_from, int file_to, char *argv[]);
-int main(int argc, char *argv[]);
 
 /**
- * error_file - Handles file-related errors.
- * @file_from: File descriptor of the source file.
- * @file_to: File descriptor of the destination file.
+ * error_file - Checks if files can be opened.
+ * @file_from: Source of file.
+ * @file_to: Destination.
  * @argv: Arguments vector.
+ *
+ * Return: Null.
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
 
 /**
- * main - Copies the contents of one file to another.
- * @argc: Number of arguments.
+ * main - Checks the code.
+ * @argc: Total number of arguments.
  * @argv: Arguments vector.
+ *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, error_close;
-	ssize_t chars, new;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
@@ -48,29 +46,30 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	chars = 1024;
-	while (chars == 1024)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		chars = read(file_from, buf, 1024);
-		if (chars == -1)
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
 			error_file(-1, 0, argv);
-		new = write(file_to, buf, chars);
-		if (new == -1)
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 
-	error_close = close(file_from);
-	if (error_close == -1)
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
-		fprintf(stderr, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	error_close = close(file_to);
-	if (error_close == -1)
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
-		fprintf(stderr, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
 }
+
